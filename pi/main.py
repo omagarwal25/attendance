e0122ad971208eed5897ad975b618a59dd97dd61
omagarwal25 <lidagarwal@gmail.com>
2022-10-23 21:56:44 +0100

    init

--- pi/main.py
+++ pi/main.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-import MFRC522
 import requests
 import RPi.GPIO as GPIO
 from dotenv import load_dotenv
+from mfrc522 import SimpleMFRC522
 
 load_dotenv()
 
@@ -12,7 +12,7 @@ API_URL = os.environ.get('API_URL')
 API_TOKEN = os.environ.get('API_TOKEN')
 
 # Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
+reader = SimpleMFRC522()
 
 # Welcome message
 print("Looking for cards")
@@ -22,16 +22,16 @@ print("Press Ctrl-C to stop.")
 try:
   while True:
     # Scan for cards
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    # (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    # (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
+    # if status == MIFAREReader.MI_OK:
      
       # Print UID
-      print(f"UID: {str(uid[0])},{str(uid[1])},{str(uid[2])},{str(uid[3])}")
+      # print(f"UID: {str(uid[0])},{str(uid[1])},{str(uid[2])},{str(uid[3])}")
 
       # Send off a signal to the server
       # RestAPI to env API_URL/pi/tap POST using requests
@@ -39,20 +39,23 @@ try:
       # Auth: Bearer token from env
 
       # hash + salt the uid
-      uid_str = f"{uid[0]}{uid[1]}{uid[2]}{uid[3]}"
-      
-      res = requests.post(f"{API_URL}/rest/tap", json={ "rfid": uid_str}, headers={ "Authorization": f"Bearer {API_TOKEN}"})
+    id, text = reader.read()
+    print(id)
+    print(text)
+    # uid_str = f"{uid[0]}{uid[1]}{uid[2]}{uid[3]}"
+    
+    # res = requests.post(f"{API_URL}/rest/tap", json={ "rfid": uid_str}, headers={ "Authorization": f"Bearer {API_TOKEN}"})
 
 
-      if res.status_code != 200:
-        # color the LEDs 
-        print(f"Error: {res.status_code}")
-      else:
-        is_tap_in: bool = res.json().get('start')
-        # whatever LEDs or displays we want to use
+    # if res.status_code != 200:
+    #   # color the LEDs 
+    #   print(f"Error: {res.status_code}")
+    # else:
+    #   is_tap_in: bool = res.json().get('start')
+    #   # whatever LEDs or displays we want to use
 
-      time.sleep(1)
-      # Reset LEDs
+    # time.sleep(1)
+    # # Reset LEDs
 
 except KeyboardInterrupt:
   GPIO.cleanup()
