220f8accc38fe02823fa9acb70c64750d307ae6c
omagarwal25 <lidagarwal@gmail.com>
2022-10-28 12:48:11 +0100

    index page fixed

--- dash/src/pages/_app.tsx
+++ dash/src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/app";
+import Head from "next/head";
 import { Nav } from "~components/Nav";
 import "~styles/globals.css";
 import { trpc } from "~utils/trpc";
@@ -12,6 +13,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Griffins Attendance</title>
+        <meta name="description" content="Tap in and Out :)" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </SessionProvider>
