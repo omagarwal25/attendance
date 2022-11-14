a57ca3616f1e471d8cb68007af1f26f74c026f57
omagarwal25 <lidagarwal@gmail.com>
2022-11-14 17:09:37 +0000

    quick patch

--- dash/src/pages/index.tsx
+++ dash/src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Create T3 App</title>
+        <title>Griffins Attendance</title>
       </Head>
       <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
         <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
