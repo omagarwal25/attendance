58550dbda9a90c1e3410a955e243d258464d009b
omagarwal25 <lidagarwal@gmail.com>
2022-11-21 20:38:02 +0000

    added RGB LED

--- pi/main.py
+++ pi/main.py
@@ -6,31 +6,31 @@ import RPi.GPIO as GPIO
 from dotenv import load_dotenv
 from mfrc522 import MFRC522
 
-# class RGB:
-#     def __init__(self, rPin: int, gPin: int, bPin: int) -> None:
-#         self.rPin = rPin
-#         self.gPin = gPin
-#         self.bPin = bPin
 
-#         GPIO.setmode(GPIO.BCM)
-#         # GPIO.setwarnings(False)
-#         GPIO.setup(self.rPin, GPIO.OUT)
-#         GPIO.setup(self.gPin, GPIO.OUT)
-#         GPIO.setup(self.bPin, GPIO.OUT)
+class RGB:
+    def __init__(self, rPin: int, gPin: int, bPin: int) -> None:
+        self.rPin = rPin
+        self.gPin = gPin
+        self.bPin = bPin
 
-#     def setColor(self, r: int, g: int, b: int) -> None:
-#         GPIO.output(self.rPin, r)
-#         GPIO.output(self.gPin, g)
-#         GPIO.output(self.bPin, b)
+        GPIO.setmode(GPIO.BCM)
+        # GPIO.setwarnings(False)
+        GPIO.setup(self.rPin, GPIO.OUT)
+        GPIO.setup(self.gPin, GPIO.OUT)
+        GPIO.setup(self.bPin, GPIO.OUT)
 
-#     def turnOff(self) -> None:
-#         GPIO.output(self.rPin, 0)
-#         GPIO.output(self.gPin, 0)
-#         GPIO.output(self.bPin, 0)
+    def setColor(self, r: int, g: int, b: int) -> None:
+        GPIO.output(self.rPin, r)
+        GPIO.output(self.gPin, g)
+        GPIO.output(self.bPin, b)
 
-led = 21
+    def turnOff(self) -> None:
+        GPIO.output(self.rPin, 0)
+        GPIO.output(self.gPin, 0)
+        GPIO.output(self.bPin, 0)
+
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led, GPIO.OUT)
 
 load_dotenv()
 
@@ -46,7 +46,7 @@ print("Press Ctrl-C to stop.")
 print(API_URL)
 print(API_TOKEN)
 
-# RGBLight = RGB(26, 20, 21)
+RGBLight = RGB(26, 21, 20)
 
 # This loop checks for chips. If one is near it will get the UID
 try:
@@ -68,30 +68,28 @@ try:
 
             print(f"UID: {uid_string}")
 
-            GPIO.output(led, 1)
-            
+            # Yellow Light
+            RGBLight.setColor(1, 1, 0)
+
             res = requests.post(f"{API_URL}/rest/tap", json={"rfid": uid_string},
                                 headers={"Authorization": f"Bearer {API_TOKEN}"})
 
             if res.status_code != 200:
-                # RGBLight.setColor(255, 0, 0)
-                print(f"Error: {res.status_code}")
-
-                GPIO.output(led, 0)
+                # Red Flash
+                RGBLight.setColor(1, 0, 0)
                 time.sleep(0.2)
-                GPIO.output(led, 1)
+                RGBLight.turnOff()
+                print(f"Error: {res.status_code}")
             else:
                 is_tap_in: bool = res.json().get('start')
-                print(is_tap_in)
-                # if is_tap_in:
-                # RGBLight.setColor(0, 255, 0)
-                # else:
-                # RGBLight.setColor(0, 0, 255)
+                if is_tap_in:
+                    RGBLight.setColor(0, 1, 0)
+                else:
+                    RGBLight.setColor(0, 0, 1)
 
-            time.sleep(1)
-            # RGBLight.turnOff()
+                time.sleep(0.2)
+                RGBLight.turnOff()
 
-            GPIO.output(led, 0)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
