91536ef329ec0d88462927e7fb127298d0840c2f
omagarwal25 <lidagarwal@gmail.com>
2023-09-16 14:52:25 +0100

    flexbox no workie

--- dash/src/pages/admin.tsx
+++ dash/src/pages/admin.tsx
@@ -62,7 +62,7 @@ export default function AdminPage() {
       <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
         <p className="flex flex-col gap-2">
           <h1 className="text-2xl">All Sessions</h1>
-          <p className="flex gap-2">
+          <p className="flex gap-2 items-center">
             <ConfirmationModal
               title="Delete All Sessions?"
               confirmButtonClass="bg-red-500 text-white"
